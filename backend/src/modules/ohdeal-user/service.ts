4ba6c77ebc1c3031b4d5887cd57e6da7f40222e0
ohdeal <setup@ohdeal.ch>
2025-07-28 07:03:17 +0200

    Update service.ts

--- backend/src/modules/ohdeal-user/service.ts
+++ backend/src/modules/ohdeal-user/service.ts
@@ -1,1 +1,22 @@
-// 
+import { EntityManager } from "typeorm"
+import { UserRole } from "./user-role.entity"
+
+class OhDealUserService {
+  private manager: EntityManager
+
+  constructor({ manager }) {
+    this.manager = manager
+  }
+
+  async setRole(userId: string, role: string) {
+    // Attribue le rôle à un user
+    await this.manager.save(UserRole, { user_id: userId, role })
+  }
+
+  async getRole(userId: string) {
+    const userRole = await this.manager.findOne(UserRole, { where: { user_id: userId } })
+    return userRole?.role
+  }
+}
+
+export default OhDealUserService
